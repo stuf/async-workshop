3f9056fa12b2b56cd0299598f6b0b533a9dfe70d
Stefan Rimaila <stefan@rimaila.fi>
2017-09-28 20:08:29 +0300

    Add exercises

---
+++ public/exercise-mapping.js
@@ -0,0 +1,44 @@
+/**
+ * EXERCISE
+ * Mapping values and errors
+ *
+ * NOTE
+ * There exists two Observables for you to use as sources in this exercise.
+ * `tick` is an observable emitting the current time every second
+ * `tickError` is an observable emitting the current time every second as error
+ */
+
+// DO NOT TOUCH
+// ------------
+
+const setOriginalTime = setIntoElement('original');
+const setModifiedTime = setIntoElement('modified');
+
+const setOriginalErrorTime = setIntoElement('original-error');
+const setModifiedErrorTime = setIntoElement('modified-error');
+
+// BEGIN EXERCISE
+// --------------
+
+/**
+ * Use the `tick` observable to print out the current time in a format you choose.
+ * You can use `date.toTimeString()`, or if you want to get creative,
+ * you can use existing `Date` methods like `getSeconds` and `getHours`
+ */
+const modified = undefined;
+
+/**
+ * Use the `tickError` observable to print out the current time, like above, but
+ * add the string "ERROR" in front of it.
+ *
+ * Remember that you can join two strings together with `+`: "a string" + "another string"
+ */
+const modifiedError = undefined;
+
+/**
+ * Finally,
+ */
+// tick.observe();
+// modified.observe();
+// tickError.observe();
+// modifiedError.observe();
--- public/vendor.js
+++ public/vendor.js
@@ -6,10 +6,17 @@ console.table({ Kefir: { library: Kefir }, ramda: { library: R } });
 
 //
 
-const setIntoElement = (id, text) =>
-  document.getElementById(id).innerText = text;
+const setIntoElement = R.curry((id, text) =>
+  document.getElementById(id).innerText = text);
 
 console.info('Additionally, there are some basic utility functions that you can use:')
 console.info({ setIntoElement });
+
+const tick = Kefir.fromPoll(1000, () => new Date());
+
+const tickError = Kefir.fromPoll(1000, () => new Date())
+                       .flatMap(Kefir.constantError);
+
+Object.assign(window, { setIntoElement, tick, tickError });
 
 console.groupEnd();
